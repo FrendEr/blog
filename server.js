30388411f5fdff89bd71858a0082a744a6e5d660
FrendEr <frend.wong@gmail.com>
2019-04-25 14:33:07 +0800

    depoy

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var spawn = require('child_process').spawn;
-free = spawn('hexo', ['server', '-p 3000']);/* 其实就是等于执行hexo server -p 3000*/
+free = spawn('hexo', ['server', '-p 4000']);/* 其实就是等于执行hexo server -p 4000*/
 
 free.stdout.on('data', function (data) {
     console.log('hexo standard output:\n' + data);
